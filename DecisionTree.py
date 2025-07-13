bb9d42ab101607256e2e498e358fd8f04c29f98a
Suneesh Jacob <suneeshjacob@gmail.com>
2025-07-13 07:48:51 +0530

    Create DecisionTree.py

---
+++ DecisionTree.py
@@ -0,0 +1,36 @@
+import numpy
+import numpy as np
+
+dataset = [['1', 'Sunny', 'Hot', 'High', 'Weak', 'No'],
+ ['2', 'Sunny', 'Hot', 'High', 'Strong', 'No'],
+ ['3', 'Overcast', 'Hot', 'High', 'Weak', 'Yes'],
+ ['4', 'Rainy', 'Mild', 'High', 'Weak', 'Yes'],
+ ['5', 'Rainy', 'Cool', 'Normal', 'Weak', 'Yes'],
+ ['6', 'Rainy', 'Cool', 'Normal', 'Strong', 'No'],
+ ['7', 'Overcast', 'Cool', 'Normal', 'Strong', 'Yes'],
+ ['8', 'Sunny', 'Mild', 'High', 'Weak', 'No'],
+ ['9', 'Sunny', 'Cool', 'Normal', 'Weak', 'Yes'],
+ ['10', 'Rainy', 'Mild', 'Normal', 'Weak', 'Yes'],
+ ['11', 'Sunny', 'Mild', 'Normal', 'Strong', 'Yes'],
+ ['12', 'Overcast', 'Mild', 'High', 'Strong', 'Yes'],
+ ['13', 'Overcast', 'Hot', 'Normal', 'Weak', 'Yes'],
+ ['14', 'Rainy', 'Mild', 'High', 'Strong', 'No']]
+
+def IG_calc(data, feature):
+    #feature = 4
+    all_classes = list(set(data[:,-1]))
+    p_beforesplit = [numpy.sum(data[:,-1]==i) for i in all_classes]
+    s_beforesplit = sum(p_beforesplit)
+    E_beforesplit = sum([-(i/s_beforesplit)*numpy.log2(i/s_beforesplit) if (i/s_beforesplit)!=0 else 0 for i in p_beforesplit])
+    n = len(data[:,feature])
+    E_eff = 0
+    for j in set(data[:,feature]):
+        k = sum(data[:,feature]==j)
+        d = data[data[:,feature]==j]
+        p = [numpy.sum(d[:,-1]==i) for i in all_classes]
+        s = sum(p)
+        E = sum([-(i/s)*numpy.log2(i/s) if (i/s)!=0 else 0 for i in p])
+        E_eff += (k/n)*E
+    IG = E_beforesplit - E_eff
+    #print(IG)
+    return IG
